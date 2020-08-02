c415667f3a324b99d73dcf4a8bdd50ca2d59195f
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-02 18:19:04 -0500

    added api route to get return all candidates

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,25 +8,95 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
 
-
-
+  console.log('Connected to the election database.');
+});
 
 
 // // TEST GET ROUTE
 // app.get('/', (req, res) => {
 //   res.json({
-//     message: "I'll be your huckleberry"
+//     message: "I'm your huckleberry"
 //   });
 // });
 
 
-// DEFAULT RESPONSE FOR ANY OTHER REQUEST (NOT FOUND CATCH-ALL)
+// GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates 
+               WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+
+// // DELETE a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// });
+
+
+// CREATE a candidate
+const sql = 
+`INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
+});
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'Success!',
+      data: rows
+    });
+  });
+});
+
+
+
+// "CATCHALL ROUTE" >> DEFAULT RESPONSE FOR ANY OTHER REQUEST (i.e. NOT FOUND)
 app.use((req, res) => {
   res.status(404).end();
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server is locked 'n' loaded on port ${PORT}`);
+
+/* To ensure that the Express.js server doesn't start before the connection 
+to the database has been established, let's wrap the Express.js server connection 
+located at the bottom of the server.js file in an event handler */
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server is locked 'n' loaded on port ${PORT}`);
+  });
 });
